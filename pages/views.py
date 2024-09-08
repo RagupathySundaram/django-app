3d46bc8a0b2a4a4716bd8b3fc0decbdd41a7ce5a
Ragupathy Sundaram <ragupathys26@gmail.com>
2024-09-08 22:22:58 +0530

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# pages/views.py
+from django.http import HttpResponse
+
+
+def home_page_view(request):
+    return HttpResponse("Hello, World!")
---
+++ test.py
@@ -0,0 +1,31 @@
+print("Fdfdfdfd")
+print("Hello, World!")
+
+# def make_my_website:
+#  ...
+# print("All Done!00")
+
+# To create a virtual environment within this new directory
+# python -m venv <name_of_env>
+# python -m venv .venv
+# .venv\Scripts\Activate.ps1
+# (.venv) $ deactivate
+
+# $ python -m pip install --upgrade pip
+
+# python manage.py runserver
+
+# python manage.py migrate
+
+# python manage.py startapp pages
+
+# pip freeze > requirements.txt
+
+import os
+import sys
+print(os.path.dirname(sys.executable))
+
+
+# django-admin startproject django_project .
+
+# django-admin --version
